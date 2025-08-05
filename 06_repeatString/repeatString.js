b359f158232c8df93a0158056d3ef004501f8811
VictorBlascoCano <101458913+VictorBlascoCano@users.noreply.github.com>
2025-08-05 16:30:33 +0200

    some exercises done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	if (num < 0) return "ERROR";
+	let text = "";
+	for (let i = 0; i < num; i++) text += str;
+	return text;
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+	if ((typeof year != "number" && year < 0) || !Number.isInteger(year))
+		return "ERROR";
+	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
